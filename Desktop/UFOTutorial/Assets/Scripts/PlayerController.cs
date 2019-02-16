5b273dcac441cf824ae8f476530806ecde92cedb
sirityzer <sirityzer@gmail.com>
2019-02-15 20:38:02 -0500

    added pickups and text

--- Desktop/UFOTutorial/Assets/Scripts/PlayerController.cs
+++ Desktop/UFOTutorial/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
     private Rigidbody2D rb2d;
+    private int count;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        SetCountText();
     }
     void FixedUpdate()
     {
@@ -19,4 +26,23 @@ public class PlayerController : MonoBehaviour
         rb2d.AddForce(movement*speed);
     }
 
+   
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
 }
